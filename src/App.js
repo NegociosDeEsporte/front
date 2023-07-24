f7a1a0c30883cbf844e91f15ceae771cf604b084
hebertdev82 <hebertdev82@gmail.com>
2023-07-24 14:04:06 -0300

    links

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import logo from './logotipo.png';
-import './App.css';
+import logo from "./logotipo.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
-        <a className="App-link" href="https://uol.com.br" target="_blank" rel="noopener noreferrer" >
+        <a
+          className="App-link"
+          href="https://loja-negociosdeesporte.vercel.app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Loja de Artigos Esportivos
         </a>
-        <a className="App-link" href="https://terra.com.br" target="_blank" rel="noopener noreferrer" >
+        <a
+          className="App-link"
+          href="https://curso-homci.vercel.app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Curso Marcelo Homci
         </a>
-
       </header>
     </div>
   );
